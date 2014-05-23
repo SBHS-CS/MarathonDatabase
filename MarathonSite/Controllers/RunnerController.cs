f6f868a7d4255ccab5f4c0446ea2303d59814f71
Adam Tobey <adamtobey@gmail.com>
2014-05-23 14:13:42 -0400

    added controller code and search parser

---
+++ MarathonSite/Controllers/RunnerController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MarathonSite.Models;
+
+namespace MarathonSite.Controllers
+{
+    public class RunnerController : Controller
+    {
+        //
+        // GET: /Runner/
+
+        //databse connection called db
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SearchParser s)
+        {
+            List<Runner> l;
+            if (s.firstName != null)
+            {
+                if (s.lastName != null)
+                {
+                    l = (from a in runners
+                         where a.firstName == s.firstName && a.lastName == s.lastName
+                         select a).toList();
+                }
+                else
+                {
+                    l = (from a in runners
+                         where a.firstName == s.firstName || a.firstName == s.lastName
+                         select a).toList();
+                }
+            }
+            else if (s.bibNumber != null)
+            {
+                l = (from a in runners
+                     where a.bibNumber == s.bibNumber
+                     select a).toList();
+            }
+            else
+            {
+                return RedirectToAction("EmptySearch");
+            }
+
+            return RedirectToAction("SearchResults", l);
+        }
+
+        public ActionResult SearchResults(List<Runner> l)
+        {
+            if (l.Count == 0)
+            {
+                return RedirectToAction("EmptySearch");
+            }
+            else if (l.Count == 1)
+            {
+                return RedirectToAction("View", l[0]);
+            } else {
+                return View(l);
+            }
+        }
+
+        public ActionResult EmptySearch()
+        {
+            return View();
+        }
+
+        public ActionResult View(Runner r)
+        {
+            return View(r);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Runner r, bool redirect)
+        {
+            string error = Verification.Verify(r);
+            if (error == null)
+            {
+                if (redirect)
+                {
+                    return RedirectToAction("View", r);
+                }
+                else
+                {
+                    return View(r);
+                }                
+            } else {
+                ViewBag.Error = error;
+                return View(r);
+            }
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Runner r)
+        {
+            string error = Verification.Verify(r);
+            if (error == null)
+            {
+                //add runner to database
+                return RedirectToAction("View", r);
+            }
+            else
+            {
+                ViewBag.Error = error;
+                return View(r);
+            }
+        }
+
+    }
+}
---
+++ MarathonSite/Models/SearchParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace MarathonSite.Models
+{
+    public class SearchParser
+    {
+        private string matchFirstName = "^[A-z]+";
+        private string matchLastName = "[A-z]+$";
+        private string matchBibNumber = "^[0-9]$";
+
+        public string query
+        {
+            get;
+            set
+            {
+                query = value;
+                firstName = Regex.Match(matchFirstName, query).Value;
+                lastName = Regex.Match(matchLastName, query).Value;
+                bibNumber = Regex.Match(matchBibNumber, query).Value;
+            }
+        }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string bibNumber { get; set; }
+    }
+}
